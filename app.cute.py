74aec30e73cb4002f21b1daef8f8eae5f1a099c3
susmitabanerjee3816-dotcom <susmitabanerjee3816@gmail.com>
2026-08-01 22:25:09 +0530

    Add files via upload

---
+++ app.cute.py
@@ -0,0 +1,33 @@
+import streamlit as st
+#app title
+
+st.title("🌸Greeting app🌸")
+
+# Text input
+name = st.text_input("Please Enter Your Name😊")
+
+#Button
+
+if st.button("💖Greet Me"):
+    if name.strip():
+        st.balloons
+
+
+        st.markdown(f"""
+        #🌸Welcome to my heart 💟🌹, {name}!🌸
+
+        
+        a 🌹 for the 🌹
+
+
+        I'm so happy you are here!🥺😍🤗
+
+
+        💟💟💟Have an amazing day!💟💟💟
+
+       🤗 ❤ Keep Smiling ❤ 🤗
+
+       """)
+         
+    else:
+        st.warning("⚠️ Please enter your name first.")
